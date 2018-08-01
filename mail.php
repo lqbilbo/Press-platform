6db62deb44979f57b37622bd92c19354cad3569e
luoqi <zhbilbo@gmail.com>
2018-08-01 21:59:32 +0800

    use config file to replace the hard code

--- mail.php
+++ mail.php
@@ -1,6 +1,27 @@
 <?php
 header("content-type:text/html;charset=utf-8");
 ini_set("magic_quotes_runtime",0);
+function get_ini_file($file_name = "config.ini") {
+    $str = file_get_contents($file_name);
+    $ini_list = explode("\n", $str);
+    $ini_items = array();
+    foreach($ini_list as $item) {
+        $one_item = explode("=", $item);
+        if (isset($one_item[0]) && isset($one_item[1]))
+            $ini_items[trim($one_item[0])] = trim($one_item[1]);
+    }
+    return $ini_items;
+}
+
+function get_ini_item($ini_items = null, $item_name = '') {
+    if (empty($ini_items))
+        return "";
+    else
+        return $ini_items[$item_name];
+}
+
+$ini_items = get_ini_file("config.ini");
+
 require 'class.phpmailer.php';
 try {
 	$mail = new PHPMailer(true); 
@@ -17,16 +38,18 @@ try {
 	$mail->Port       = 465;//25;
 	$mail->Host       = "ssl://smtp.163.com";
 
-	$mail->Username   = "muoufunstory@163.com";
+	$mail->Username   = get_ini_item($ini_items, 'send_from_mail');
 	$mail->Password   = "Luo1988";//"T&H#163";
 	//$mail->IsSendmail(); //如果没有sendmail组件就注释掉，否则出现“Could  not execute: /var/qmail/bin/sendmail ”的错误提示
-	$mail->AddReplyTo("muoufunstory@163.com","muoufunstory@163.com");//回复地址
-	$mail->From       = "muoufunstory@163.com";
-	$mail->FromName   = "muoufunstory@163.com";
-	$to = "gelato4u@163.com";
+	$mail->AddReplyTo(
+	    get_ini_item($ini_items, 'send_from_mail'),
+        get_ini_item($ini_items, 'send_from_mail')
+    );//回复地址
+	$mail->From       = get_ini_item($ini_items, 'send_from_mail');
+	$mail->FromName   = get_ini_item($ini_items, 'send_from_mail');
+	$to = get_ini_item($ini_items, 'send_to_mail');
 	$mail->AddAddress($to);
 	$mail->Subject  = $_POST['mailsubject'];//"发送邮件测试标题";
-//	$mail->Body = "<h1>发送邮件演示</h1>这是木偶漫画工作室（<font color=red>$mail->FromName</font>），感谢您的投稿";
     $inlineBody = $_POST['mailbody'];
     $mail->Body = "<pre>$inlineBody</pre>";
     $mail->AltBody    = "请使用HTML兼容的邮件客户端"; //当邮件不支持html时备用显示，可以省略
